a1ccf0db67403830cc5f91ee18f4f463a08e9db8
Tuomas Alanen <tuomas@paaryna.fi>
2019-10-15 20:25:37 +0300

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
@@ -15,8 +17,34 @@ const favoriteBlog = (blogs) => {
   return favorite[0]
 }
 
+const mostBlogs = (blogs) => {
+  const writers = []
+  blogs.map((a) => {
+    var loytyyko = []
+    for(const writer of writers){
+      if(writer.author === a.author){
+        loytyyko.push(true)
+      }
+    }
+    if(loytyyko.length === 0){
+      const talleta = {
+        author: a.author,
+        blogs: 1
+      }
+      writers.push(talleta)
+    } else{
+      const id = writers.findIndex(b => a.author === b.author)
+      writers[id].blogs = writers[id].blogs + 1
+    }
+  })
+  writers.sort((a,b) => b.blogs-a.blogs)
+  const most = writers.filter(a => a.blogs === writers[0].blogs)
+  return most[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
